82245f35aadc2398265dc2976a83fcc9a9c09d2f
Salman <salmankunbargi@gmail.com>
2021-07-03 22:42:59 +0300

    Adding 3rd Route

--- Routes/index.js
+++ Routes/index.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const Home = require("./Defaults/Home/Home");
 const Preferances = require("./Defaults/Preferances/Preferances");
 
+const Admin = require("./Admin/Admin");
+
 router.use("/", Home);
 router.use("/Preferances", Preferances);
+
+// Admin
+router.use("/Admin", Admin);
 
 module.exports = router;
